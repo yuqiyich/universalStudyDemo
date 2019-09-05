14cf0d9d44c604acac7e79cddfa7a99b55d06bbb
yich <yuqi@ruqimobility.com>
2019-09-05 14:32:24 +0800

    testcode

--- app/src/main/java/com/example/firstdemo/TestSYSENV.java
+++ app/src/main/java/com/example/firstdemo/TestSYSENV.java
@@ -7,7 +7,10 @@ public class TestSYSENV {
     public static void main(String[] args)   {
         System.out.println("******************************Environment Vars*****************************");
         Map<String, String> enviorntmentVars  = System.getenv();
-        enviorntmentVars.entrySet().forEach(System.out::println);
+        //appVersion:300000150phone num:deviceId:99001155052774phoneBrand:OnePlus-ONEPLUS A5010androidApiVer:28channel:null
+        String rule="hitGray(%d,'%s','%s','%s',%d,'%s')";
+//        enviorntmentVars.entrySet().forEach(System.out::println);
+        System.out.println(String.format(rule,300000150,"","99001155052774","OnePlus-ONEPLUS A5010",28,null));
 
     }
 
